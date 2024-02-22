1cdf07044647e2742379f721f8dbd302f58dc7da
Tan <[tanqiwen0302@gmail.com]>
2024-02-22 18:46:59 +0000

    demo.matlab added

--- 002Matlab Project/project/Demostration.m
+++ 002Matlab Project/project/Demostration.m
@@ -5,18 +5,19 @@ clear;
 close all;
 clc;
 % INPUT
-oriImg = 'origin.png';
-triMap = 'trimapOrigin.png';
-figure;
-imshow(oriImg);
-figure;
-imshow(triMap);
+oriImg = 'troll.png';
+triMap = 'troll3.png';
+BgPath = "whiteBg.png";
+% figure;
+% imshow(oriImg);
+% figure;
+% imshow(triMap);
 % DUT
 [frontImg, backImg, unknownImg] = FunPreProcess(oriImg, triMap);
 % BENCH
-figure;
-imshow([uint8(frontImg) uint8(backImg) uint8(unknownImg)]);
-drawnow;
+% figure;
+% imshow([uint8(frontImg) uint8(backImg) uint8(unknownImg)]);
+% drawnow;
 % -------------------------------------------------
 % Test FunStatistical Analysis Unit Test (Partitial)
 % -------------------------------------------------
@@ -30,23 +31,26 @@ disp(coB);
 % -------------------------------------------------
 % INPUT
 % user input Iteration times
-Iteration = 10; % USER INPUT %
-oriVar    = 8;  % USER INPUT %
+Iteration = 100; % USER INPUT %
+oriVar    = 10;  % USER INPUT %
 [unknownAlpha, unknownF, unknownB] = Matting(unknownImg, ...
     triMap, coF, coB, Fmean, Bmean, oriVar, Iteration);
+
+figure,
+imshow([uint8(unknownAlpha*255)]);
 %%
 close all;
 mat_ori_Img = imread(oriImg);
 figure;
 imshow(mat_ori_Img);
 figure;
-imshow(unknownAlpha);
-result = double(mat_ori_Img) .* unknownAlpha;
+imshow(unknownAlpha*255);
+result = double(mat_ori_Img) .* unknownAlpha*255;
 figure;
 imshow(result);
 figure;
 imshow(unknownF);
 %%
 FThreshold =  255 * 0.95;
-GiveNewBackground(oriImg, triMap, unknownF, unknownAlpha, FThreshold);
+GiveNewBackground(oriImg, triMap, unknownF, unknownAlpha, FThreshold, BgPath);
 
