4971bb5158eb54050a586b9e30679a5309d6118a
Changhong Li <15284244183@163.com>
2024-02-27 18:12:32 +0000

    Add single demo for functions

---
+++ 005Submisson/002Week6Submission/005DemoFBC(finished)/DemoFBC.m
@@ -0,0 +1,30 @@
+% Demo alpha, C -> F, B
+% clear all data
+clc;
+clear;
+close all;
+
+load('EstimateFB_Inputs_Iter1_Pos(38,364).mat');
+
+[F, B] = EstimateFB(a, b, alpha, invcoF, invcoB, oriVar, ...
+    Fmean, Bmean, unknownImg);
+
+fprintf("F = \n");
+disp(F);
+fprintf("B = \n");
+disp(B);
+%%
+% Demo F, B, -> alpha
+% clear all data
+clc;
+clear;
+close all;
+
+load('CalculateAlpha_Inputs_Iter1_Pos(38,364).mat');
+
+C = currentPixel;
+F = tempF;
+B = tempB;
+
+alpha = CalculateAlpha(C, F, B);
+fprintf("Alpha = %f\n", alpha);
